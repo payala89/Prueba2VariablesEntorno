7660081fe535fc747f490de18fdb637696374699
payala89 <payalar.89@gmail.com>
2022-08-30 15:33:40 -0500

    Base de Datos con una prueba agregada

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
 from decouple import config
+from db import get_connection
 
-print(config('MYSQL_HOST'))
-print(config('MYSQL_USER'))
-print(config('MYSQL_PASSWORD'))
-print(config('MYSQL_DB'))
 
-print("Docs")
+try:
+    connection=get_connection()
+    with connection.cursor() as cursor:
+        query="select * from profesor where id=1;"
+        cursor.execute(query)
+        row=cursor.fetchone()
+        print(row)
+    connection.close()
+except Exception as ex:
+    print(ex)
